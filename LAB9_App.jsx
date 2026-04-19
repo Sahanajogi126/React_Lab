146bc3f1c83acd25d852bb235efbfe90493c3904
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2026-04-19 21:55:34 +0530

    Create LAB9_App.jsx

---
+++ LAB9_App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import './App.css'
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <div style={{ padding: '20px' }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
